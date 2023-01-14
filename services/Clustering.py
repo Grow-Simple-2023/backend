13c007d392fb0068e889af0d40d7b1fb5ade3a3e
pavanvpatil <pavanvpatil01@gmail.com>
2023-01-14 12:59:51 +0530

    clustering done

--- services/Clustering.py
+++ services/Clustering.py
@@ -1,11 +1,13 @@
+from typing import List,Tuple
+import random as rd
 class Clustering:
     
     item_dims: List[Tuple[float]]
     item_lat_long: List[Tuple[float]]
     no_riders: int
-    rider_vol: float
+    rider_vol: List[float]
     
-    def __init__(self, item_dims: List[Tuple[float]], item_lat_long: List[Tuple[float]], no_riders: int, rider_vol: float) -> None:
+    def __init__(self, item_dims: List[Tuple[float]], item_lat_long: List[Tuple[float]], no_riders: int, rider_vol: List[float]) -> None:
         assert len(item_dims)>0
         assert len(item_dims)==len(item_lat_long)
         assert len(item_dims[0])==3
@@ -17,10 +19,63 @@ class Clustering:
         self.no_riders = no_riders
         self.rider_vol = rider_vol
     
-    def distribute(self) -> List[List[int]]:
+    
+    
+    def distribute(self):
+        class Centroid:
+            def __init__(self,centroid,points):
+                self.centroid = centroid
+                self.points = points
+
         
-        """
-        TODO: Implement this function
-        """
+        epochs = 500
+        index = []
+        centroids = []
+        for i in range(self.no_riders):
+            while True:
+                a = rd.randint(0,len(self.item_lat_long)-1)
+                if a not in index:
+                    centroid = self.item_lat_long[a]
+                    points = []
+                    centroids.append(Centroid(centroid,points))
+                    break
         
-        pass
+        # for i in centroids:
+        #     print(i.centroid)
+        #     print(i.points)
+        for i in range(5):
+            for centroid in range(len(centroids)):
+                centroids[centroid].points = []
+            
+            for point in range(len(self.item_lat_long)):
+                min_distance  = float('inf')
+                centroid_index = 0
+                for j in range(len(centroids)):
+                    centroid = centroids[j].centroid
+                    distance = (self.item_lat_long[point][0]-centroid[0])**2+(self.item_lat_long[point][1]-centroid[1])**2
+                    if distance < min_distance:
+                        centroid_index = j
+                        min_distance = distance
+                centroids[centroid_index].points.append((point,self.item_lat_long[point]))
+
+            # for k in centroids:
+            #     print(k.centroid)
+            #     print(k.points)
+            
+            for centroid in range(len(centroids)):
+                lat = 0
+                long = 0
+                for point in centroids[centroid].points:
+                    lat += point[1][0]
+                    long += point[1][1]
+                size = len(centroids[centroid].points)
+                if size != 0:
+                    lat = lat/size
+                    long = long/size
+                    centroids[centroid].centroid = (lat,long)
+
+        for i in centroids:
+            print(i.centroid)
+            print(i.points)    
+
+                
---
+++ services/generateforclustering.py
@@ -0,0 +1,52 @@
+from Clustering import Clustering
+import random
+
+# import matplotlib.pyplot as plt
+
+no_of_riders = 5
+no_of_deliveries = 50
+
+item_lat_long = []
+item_dims = []
+riders_vol = []
+
+x = []
+y = []
+for i in range(no_of_deliveries):
+    lat = random.uniform(15,30)
+    long = random.uniform(75,100)
+    x.append(lat)
+    y.append(long)
+    item_lat_long.append((lat,long))
+
+for i in range(no_of_deliveries):
+    l = random.uniform(10,20)
+    b = random.uniform(10,20)
+    h = random.uniform(10,20)
+    item_dims.append((l,b,h))
+
+for i in range(no_of_riders):
+    riders_vol.append(random.uniform(75000,400000))
+
+
+cluster = Clustering(item_dims,item_lat_long,no_of_riders,riders_vol)
+
+
+# index = []
+# x1 = []
+# y1 = []
+# for i in range(no_of_riders):
+#             while True:
+#                 a = random.randint(0,len(item_lat_long)-1)
+#                 if a not in index:
+#                     x1.append( item_lat_long[a][0])
+#                     y1.append(item_lat_long[a][1])
+#                     break
+
+# plt.scatter(x,y)
+# plt.scatter(x1,y1)
+# plt.show()
+cluster.distribute()
+
+
+
