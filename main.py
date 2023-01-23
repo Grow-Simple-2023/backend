2d7f550ed62de3a134f09b00670168662baea8c5
Shashank P <shashankp2832@gmail.com>
2023-01-23 21:52:00 +0530

    Added cross origin error solve

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
 from routes import manager, rider, customer, generic
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(generic.router, tags=["General"])
 app.include_router(manager.router, prefix="/manager", tags=["Manager"])
