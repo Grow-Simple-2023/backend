4749645ac29a27b22c2ee54135444a41e3f86d57
pavanvpatil <pavanvpatil01@gmail.com>
2023-01-26 20:02:41 +0530

    deleted customer

--- main.py
+++ main.py
@@ -16,4 +16,3 @@ app.add_middleware(
 app.include_router(generic.router, tags=["General"])
 app.include_router(manager.router, prefix="/manager", tags=["Manager"])
 app.include_router(rider.router, prefix="/rider", tags=["Rider"])
-app.include_router(customer.router, prefix="/customer", tags=["Customer"])
--- routes/customer.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-
-@router.get("/")
-async def customer_home():
-    return {"message": "Welcome to Grow-Simplee Customer API"}
