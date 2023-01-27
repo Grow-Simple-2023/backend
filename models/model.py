62ae13bcd1eb58bc9da8e1c7cb7189c119f14d07
pavanvpatil <pavanvpatil01@gmail.com>
2023-01-27 19:57:44 +0530

    endpoint item_status_update done

--- models/model.py
+++ models/model.py
@@ -5,3 +5,8 @@ class DistributeModel(BaseModel):
     item_ids: List[str]
     rider_phone_nos: List[str]
     
+class ItemStatusModel(BaseModel):
+    item_id: str
+    status: int
+    OTP: int
+    phone_no: str
