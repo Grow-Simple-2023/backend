7dfbd963c5aec84726873f24b98c97e64e67ec5f
Shashank P <shashankp2832@gmail.com>
2023-02-02 00:39:12 +0530

    Added LDAP login and role with jwt

--- routes/generic.py
+++ routes/generic.py
@@ -19,26 +19,33 @@ async def generic_home():
 
 @router.post("/register")
 async def register(register: Register):
-    if register.password!=register.confirm_password:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Passwords do not match")
-    if len(register.password)<8:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Password length must be atleast 8")
+    if register.password != register.confirm_password:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Passwords do not match")
+    if len(register.password) < 8:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Password length must be atleast 8")
     token = Token(**{"access_token": "encoded", "token_type": "Bearer"})
-    if(not register.phone_no.isnumeric() or len(register.phone_no) != 10):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Provide correct phone number")
+    if (not register.phone_no.isnumeric() or len(register.phone_no) != 10):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Provide correct phone number")
     check_user_exists(register.phone_no)
-    user = add_new_user(phone_no=register.phone_no, first_name=register.first_name, last_name=register.last_name, password=register.password, role=register.role)
+    user = add_new_user(phone_no=register.phone_no, first_name=register.first_name,
+                        last_name=register.last_name, password=register.password, role=register.role)
     token = create_token(phone_no=user["phone_no"], role=user["role"])
-    return {"user": user, 
-            "auth": token} 
+    return {"user": user,
+            "auth": token}
+
 
 @router.post("/login")
 async def login(login: Login):
-    if(not login.phone_no.isnumeric() or len(login.phone_no) != 10):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Provide correct phone number")
+    if (not login.phone_no.isnumeric() or len(login.phone_no) != 10):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Provide correct phone number")
     user = verify_credentials(phone_no=login.phone_no, password=login.password)
     token = create_token(phone_no=user["phone_no"], role=user["role"])
-    return token
+    return {"token": token, "role": user["role"]}
+
 
 @router.get("/decode-token")
 async def decode_token(load: str = Depends(decode_jwt)):
