c32c681144ea56f45ba30ae7b06d796a3d93951a
Shashank P <shashankp2832@gmail.com>
2023-02-02 16:31:14 +0530

    Optimized few more queries

--- routes/rider.py
+++ routes/rider.py
@@ -9,98 +9,87 @@ router = APIRouter()
 
 
 @router.get("/")
-async def rider_home(user_data = Depends(decode_jwt)):
+async def rider_home(user_data=Depends(decode_jwt)):
     check_role(user_data, ["ADMIN", "RIDER"])
     return {"message": "Welcome to Grow-Simplee Rider API"}
 
+
 @router.get("/route/{phone_no}")
-async def get_route_by_number(phone_no: str, user_data = Depends(decode_jwt)):
+async def get_route_by_number(phone_no: str, user_data=Depends(decode_jwt)):
     check_role(user_data, ["ADMIN", "RIDER"])
     route = db.route.find_one({"rider_id": phone_no}, {"_id": 0})
-    if not route: raise HTTPException(status_code=404, detail=f"Rider not assigned to a route: {phone_no}")
+    if not route:
+        raise HTTPException(
+            status_code=404, detail=f"Rider not assigned to a route: {phone_no}")
     return {"route": route}
 
+
 @router.post("/item-status-update")
-async def item_status_update(item_status: ItemStatusModel, user_data = Depends(decode_jwt)):
+async def item_status_update(item_status: ItemStatusModel, user_data=Depends(decode_jwt)):
     check_role(user_data, ["ADMIN", "RIDER"])
-    rider_with_item_details = db.route.find_one({"items_in_order.id": item_status.item_id,
-                                                 "rider_id":user_data.phone_no}, {"_id": 0})
-    if not rider_with_item_details:
-         raise HTTPException(status_code=404, detail="Item not assigned to " + str(user_data.phone_no)+" rider")
-    OTP = db.item.find_one({"id": item_status.item_id}, {"_id": 0,"OTP":1})
-    if(OTP["OTP"] == item_status.OTP):
-        if(item_status.status==1):
-            db.item.update_one({"id": item_status.item_id}, {"$set": {"control.is_fulfilled": True,
-                                                                      "control.is_assigned": False,
-                                                                      "control.is_cancelled": False,
-                                                                      "delivered_on": str(datetime.now())}})
-            db.route.update_one({"rider_id": user_data.phone_no}, {"$pull": {"items_in_order": 
-                                                                               {"id": item_status.item_id}}})
-            item = db.item.find_one({"id": item_status.item_id}, {"_id": 0})
-            return item
-        elif item_status.status==0:
-            db.item.update_one({"id": item_status.item_id}, {"$set": {"control.is_cancelled": True,"control.is_fulfilled": False,"control.is_pickup": False,"control.is_assigned":False,"control.is_delivery":False}})
-            db.route.update_one({"rider_id": user_data.phone_no}, {"$pull": {"items_in_order": 
-                                                                               {"id": item_status.item_id}}})
-            db.route.update_one({"rider_id": user_data.phone_no}, {"$set": {"last_modified": str(datetime.now())}})
-            item = db.item.find_one({"id": item_status.item_id}, {"_id": 0})
-            return {"updated_item": item}
-        else:
-            raise HTTPException(status_code=404, detail="Status error")
-    else:
+    rider = db.route.find_one(
+        {"items_in_order.id": item_status.item_id, "rider_id": user_data.phone_no}, {"_id": 0})
+    if not rider:
+        raise HTTPException(
+            status_code=404, detail="Item not assigned to " + str(user_data.phone_no) + " rider")
+
+    item = db.item.find_one({"id": item_status.item_id}, {"_id": 0, "OTP": 1})
+    if not item:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    if item["OTP"] != item_status.OTP:
         raise HTTPException(status_code=404, detail="OTP does not match")
 
-@router.delete("/end-route")
-async def end_route(route_end_model: RouteEndModel, user_data = Depends(decode_jwt)):
-    check_role(user_data, ["ADMIN", "RIDER"])
-    phone_no = user_data["phone_no"]
-    route_info = db.route.find_one({"rider_id": phone_no, 
-                                    "route_otp": route_end_model.route_otp}, {"_id": 0})
-    
-    if not route_info:
-        raise HTTPException(status_code=404, detail=f"Wrong OTP / Item not Assigned: {phone_no}")
-    db.route.delete_one({"rider_id": phone_no})
-    for item in route_info["items_in_order"]:
-        db.item.update_one({"id": item["id"]}, {"$set": {"control.is_assigned": False,
-                                                        "control.is_fulfilled": False,
-                                                        "conrtol.is_cancelled": False}})
-    route_info["route_end_time"] = str(datetime.now())
-    db.route_archive.insert_one(route_info)
-    return {"deleted_route": route_info}
+    if item_status.status not in [0, 1]:
+        raise HTTPException(status_code=404, detail="Status error")
+
+    control = {"is_fulfilled": item_status.status == 1,
+               "is_cancelled": item_status.status == 0, "is_assigned": False}
+    db.item.update_one({"id": item_status.item_id}, {
+                       "$set": {"control": control, "delivered_on": str(datetime.now())}})
+
+    db.route.update_one({"rider_id": user_data.phone_no},
+                        {"$pull": {"items_in_order": {"id": item_status.item_id}},
+                         "$set": {"last_modified": str(datetime.now())}})
+
+    item = db.item.find_one({"id": item_status.item_id}, {"_id": 0})
+    return {"updated_item": item} if item_status.status == 0 else item
+
 
 @router.put("/modify-route")
-async def modify_route(rider_id: str, item_ids_in_order: List[str], user_data = Depends(decode_jwt)):
+async def modify_route(rider_id: str, item_ids_in_order: List[str], user_data=Depends(decode_jwt)):
     check_role(user_data, ["ADMIN", "RIDER"])
-    if(role=="RIDER"):
-        if(rider_id != user_data["phone_no"]):
-            raise HTTPException(status_code=404, detail="You are not authorized to modify this route")
+    if (role == "RIDER"):
+        if (rider_id != user_data["phone_no"]):
+            raise HTTPException(
+                status_code=404, detail="You are not authorized to modify this route")
     existing_route = db.route.find_one({"rider_id": rider_id}, {"_id": 0})
     if not existing_route:
-        raise HTTPException(status_code=404, detail=f"Route not found: {rider_id}")
+        raise HTTPException(
+            status_code=404, detail=f"Route not found: {rider_id}")
     existing_order = existing_route["items_in_order"]
-    new_order = []
-    for order in item_ids_in_order:
-        for item in existing_order:
-            if item["id"] == order:
-                new_order.append(item)
+    existing_order_dict = {item["id"]: item for item in existing_order}
+
     try:
+        new_order = [existing_order_dict[id] for id in item_ids_in_order]
         assert len(new_order) == len(existing_order)
     except:
-        raise HTTPException(status_code=404, detail=f"Invalid order: {rider_id}")
-    db.route.update_one({"rider_id": rider_id}, {"$set": {"items_in_order": new_order}})
+        raise HTTPException(
+            status_code=404, detail=f"Invalid order: {rider_id}")
+    db.route.update_one({"rider_id": rider_id}, {
+                        "$set": {"items_in_order": new_order, "last_modified": str(datetime.now())}})
     return {"items_in_route": db.route.find_one({"rider_id": rider_id}, {"_id": 0})}
 
+
 @router.get("/is-route-modified-after/{route_id}")
-async def is_route_modified_after(rider_id: str, last_modified: str, user_data = Depends(decode_jwt)):
+async def is_route_modified_after(route_id: str, last_modified: str, user_data=Depends(decode_jwt)):
     check_role(user_data, ["ADMIN", "RIDER"])
-    if(role=="RIDER"):
-        if(rider_id != user_data["phone_no"]):
-            raise HTTPException(status_code=404, detail="You are not authorized to modify this route")
-    existing_route = db.route.find_one({"rider_id": rider_id}, {"_id": 0})
+    if (user_data["role"] == "RIDER" and route_id != user_data["phone_no"]):
+        raise HTTPException(
+            status_code=404, detail="You are not authorized to modify this route")
+    existing_route = db.route.find_one(
+        {"rider_id": route_id}, {"_id": 0, "last_modified": 1})
     if not existing_route:
-        raise HTTPException(status_code=404, detail=f"Route not found: {rider_id}")
-    if existing_route["last_modified"] > last_modified:
-        return {"is_modified": True}
-    return {"is_modified": False}
-    
-    
+        raise HTTPException(
+            status_code=404, detail=f"Route not found: {route_id}")
+    return {"is_modified": existing_route["last_modified"] > last_modified}
