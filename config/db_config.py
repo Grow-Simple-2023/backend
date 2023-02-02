3fde1ae7197183069f0bae702248dc48ea37c026
shashankp28 <shashankp2832@gmail.com>
2023-02-03 00:03:19 +0530

    Added screen feature

--- config/db_config.py
+++ config/db_config.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 username = os.getenv("MONGO_ROOT_USERNAME")
 password = os.getenv("MONGO_ROOT_PASSWORD")
-connection_url = f"mongodb://{username}:{password}@mongodb:27017"
+connection_url = f"mongodb://{username}:{password}@localhost:27018"
 client = MongoClient(connection_url)
 db = client.get_database("grow-simplee")
