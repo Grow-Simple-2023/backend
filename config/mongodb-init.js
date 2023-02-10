d8f52ecd3218a52d069d1b67e02fa36d01861bf5
Shashank P <shashankp2832@gmail.com>
2023-02-10 15:16:17 +0530

    initialize with hub

--- config/mongodb-init.js
+++ config/mongodb-init.js
@@ -1,12 +1,11 @@
-db = db.getSiblingDB('grow-simplee');
-db.createCollection('user');
-db.createCollection('item');
-db.createCollection('route');
-db.createCollection('route_archive');
-
+db = db.getSiblingDB("grow-simplee");
+db.createCollection("user");
+db.createCollection("item");
+db.createCollection("route");
+db.createCollection("route_archive");
 
 const user = require("/user.json");
-const item = require("/item.json");
+const hub = require("/hub.json");
 
 db.user.insertMany(user);
-db.item.insertMany(item);
+db.item.insertMany(hub);
